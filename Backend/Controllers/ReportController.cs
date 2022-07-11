6f312ff00c6947c0bc39bf62f70655ad8ff95e3b
gilbertkibet <kibetgilly354@gmail.com>
2022-07-11 22:38:44 +0300

    added reports file system

---
+++ Backend/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend.Utilities;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Backend.Controllers
+{
+    public class ReportController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("Reports/CrystalReport")]
+        [HttpGet]
+        [ClientCacheWithEtag(60)]  //1 min client side caching
+        public HttpResponseMessage CrytalReport()
+        {
+            string reportPath = "~/Reports";
+            string reportFileName = "CrystalReport.rpt";
+            string exportFilename = "CrystalReport.pdf";
+
+            HttpResponseMessage result = CrystalReport.RenderReport(reportPath, reportFileName, exportFilename);
+            return result;
+        }
+    }
+}
